fec6bc83511a3b95b1db85c9327410594d9d5337
kk3939 <kyo51310@gmail.com>
2022-02-08 22:03:41 +0900

    test: unit test　clear

--- src/Primes.hs
+++ src/Primes.hs
@@ -1,17 +1,15 @@
 module Primes where
 
-isPrime :: Int -> Maybe Bool 
-isPrime n | n < 0 = Nothing
-  | n >= last primes = Nothing
-  | otherwise = Just (n `elem` primes)
-
-primes :: [Int]
-primes = [1..10000]
-
-
 sieve :: [Int]->[Int]
 sieve [] = []
-sieve(nextPrime : rest) = nextPrime : sieve noFactors
- where noFactors = filter(not.(==0).(`mod`nextPrime)) rest
+sieve(nextPrime:rest) = nextPrime:sieve noFactors
+ where noFactors = filter(not.(==0).(`mod`nextPrime))rest
+
+primes :: [Int]
+primes = sieve[2..10000]
 
 
+isPrime :: Int -> Maybe Bool 
+isPrime n | n < 2 = Nothing
+  | n >= last primes = Nothing
+  | otherwise = Just(n `elem` primes)
